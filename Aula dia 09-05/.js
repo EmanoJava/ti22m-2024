6ecd5137d38b980b8a52e644d389b73fedfe856c
EmanoJava <163414137+EmanoJava@users.noreply.github.com>
2024-09-05 09:55:33 -0300

    aula mes 9

---
+++ Aula dia 09-05/.js
@@ -0,0 +1,36 @@
+function SistemaDeMultas(){
+    let velmax = parseFloat(document.querySelector('').value); 
+    let velvei = parseFloat(document.querySelector('').value);
+
+    
+    if (velvei > velmax * (0,20)){
+
+        multa = "R$ 130,16"
+
+    }
+
+    else{
+
+        multa = "R$ 00,00"
+
+    }
+
+    if( valvei > velmax * (20,50)){
+
+        multa = "R$ 195,23"
+
+    }
+
+    else{
+
+        multa = "R$ 00,00"
+
+    }
+
+    if( valvei > velmax * )
+
+
+
+
+
+}
